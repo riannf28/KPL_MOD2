a9b942ab4181bbe3dcf90bd4948f305822292a05
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-05 16:53:09 +0700

    menambahkan implementasi kalkulator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,6 +52,7 @@ namespace modul2_1302200040
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -61,6 +62,7 @@ namespace modul2_1302200040
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -70,6 +72,7 @@ namespace modul2_1302200040
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -79,6 +82,7 @@ namespace modul2_1302200040
             this.button4.TabIndex = 3;
             this.button4.Text = "+";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -88,6 +92,7 @@ namespace modul2_1302200040
             this.button5.TabIndex = 4;
             this.button5.Text = "9";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -97,6 +102,7 @@ namespace modul2_1302200040
             this.button6.TabIndex = 5;
             this.button6.Text = "8";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -106,6 +112,7 @@ namespace modul2_1302200040
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -115,6 +122,7 @@ namespace modul2_1302200040
             this.button8.TabIndex = 7;
             this.button8.Text = "6";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -124,6 +132,7 @@ namespace modul2_1302200040
             this.button9.TabIndex = 8;
             this.button9.Text = "5";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -133,6 +142,7 @@ namespace modul2_1302200040
             this.button10.TabIndex = 9;
             this.button10.Text = "4";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -142,6 +152,7 @@ namespace modul2_1302200040
             this.button11.TabIndex = 10;
             this.button11.Text = "0";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -151,15 +162,15 @@ namespace modul2_1302200040
             this.button12.TabIndex = 11;
             this.button12.Text = "=";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(115, 82);
+            this.label1.Location = new System.Drawing.Point(161, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.Size = new System.Drawing.Size(0, 20);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Label Output";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,10 @@ namespace modul2_1302200040
 {
     public partial class Form1 : Form
     {
+        decimal bil1;
+        decimal bil2;
+        int opr;
+        Boolean opr_done = false;
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +25,141 @@ namespace modul2_1302200040
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(label1.Text == "0")
+            {
+                label1.Text = "1";
+            } else {
+                label1.Text += "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "2";
+            }
+            else
+            {
+                label1.Text += "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "3";
+            }
+            else
+            {
+                label1.Text += "3";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "4";
+            }
+            else
+            {
+                label1.Text += "4";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "5";
+            }
+            else
+            {
+                label1.Text += "5";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "6";
+            }
+            else
+            {
+                label1.Text += "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "7";
+            }
+            else
+            {
+                label1.Text += "7";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "8";
+            }
+            else
+            {
+                label1.Text += "8";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "0")
+            {
+                label1.Text = "9";
+            }
+            else
+            {
+                label1.Text += "9";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if(label1.Text != "0")
+            {
+                label1.Text += "0";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bil1 = Convert.ToDecimal(label1.Text);
+            label1.Text = " ";
+            opr = 1;
+            opr_done = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (opr_done == true)
+            {
+                bil2 = Convert.ToDecimal(label1.Text);
+                label1.Text = Convert.ToString(bil1 + bil2);
+
+            }
+            opr_done = false;
+            }
+        }
+
     }
-}
+
+
